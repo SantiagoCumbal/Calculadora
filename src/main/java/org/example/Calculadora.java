b9fc503c327cb9550ea92e22ea1856fe2d05836f
SantiagoCumbal <santiago.cumbal@epn.edu.ec>
2024-06-25 23:31:54 -0500

    basico

--- src/main/java/org/example/Calculadora.java
+++ src/main/java/org/example/Calculadora.java
@@ -1,18 +1,71 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Calculadora {
-    private JPanel mainPanel;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
+    public JPanel mainPanel;
+    private JTextField numero1;
+    private JTextField numero2;
+    private JButton sumabutton;
+    private JButton restabutton;
+    private JButton multibutton;
     private JButton raizButton;
     private JButton potenciaButton;
-    private JButton button6;
+    private JButton divibutton;
     private JButton secButton;
     private JButton cosButton;
     private JButton tanButton;
+    private JLabel Respuesta;
+
+    public Calculadora() {
+        sumabutton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int numero1V=Integer.parseInt(numero1.getText());
+                int numero2V=Integer.parseInt(numero2.getText());
+                int suma=numero1V+numero2V;
+                Respuesta.setText(suma+"");
+            }
+        });
+        restabutton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int numero1V=Integer.parseInt(numero1.getText());
+                int numero2V=Integer.parseInt(numero2.getText());
+                int resta=numero1V-numero2V;
+                Respuesta.setText(resta+"");
+            }
+        });
+        multibutton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int numero1V=Integer.parseInt(numero1.getText());
+                int numero2V=Integer.parseInt(numero2.getText());
+                int multi=numero1V*numero2V;
+                Respuesta.setText(multi+"");
+            }
+        });
+
+        divibutton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double numero1V=Integer.parseInt(numero1.getText());
+                double numero2V=Integer.parseInt(numero2.getText());
+                if(numero2V==0){
+                    Respuesta.setText("NO EXISTE DIVISION PARA 0");
+                } else{
+                    double divi=numero1V/numero2V;
+                    Respuesta.setText(divi+"");
+                }
+            }
+        });
+        raizButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,16 @@
 package org.example;
 
+import javax.swing.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        JFrame calcu = new JFrame("Calculadora");
+        calcu.setContentPane(new Calculadora().mainPanel);
+        calcu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        calcu.setSize(400,200);
+        calcu.pack();
+        calcu.setVisible(true);
     }
 }

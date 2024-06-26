a610b2be3b2b7b4c16227d9e805509506a0fc3a7
PaulCabreraEPN <paul.cabrera@epn.edu.ec>
2024-06-25 22:22:08 -0500

    v1 Calculadora

--- src/main/java/org/example/Calculadora.java
+++ src/main/java/org/example/Calculadora.java
@@ -4,14 +4,15 @@ import javax.swing.*;
 
 public class Calculadora {
     private JPanel mainPanel;
-    private JTextField numero1;
-    private JTextField numero2;
+    private JTextField textField1;
+    private JTextField textField2;
     private JButton button1;
     private JButton button2;
     private JButton button3;
     private JButton button4;
-    private JButton button5;
-    private JButton button6;
-    private JButton button7;
-    private JButton button8;
+    private JButton raizButton;
+    private JButton potenciaButton;
+    private JButton senButton;
+    private JButton tanButton;
+    private JButton cosButton;
 }

f358253a99f9a6dd877ba4cd92ea7b1f00dd615b
SantiagoCumbal <santiago.cumbal@epn.edu.ec>
2024-06-25 20:08:41 -0500

    Primera Base

---
+++ src/main/java/org/example/Calculadora.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Calculadora {
+    private JPanel mainPanel;
+    private JTextField numero1;
+    private JTextField numero2;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+}
